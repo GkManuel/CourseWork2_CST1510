78d66b169538b9589c2c20f505c60a83d9c8799f
GkManuel <emanuel.vuhahula@gmail.com>
2025-11-05 15:12:26 +0400

    Update: Created a function to verify password

--- authorize.py
+++ authorize.py
@@ -1,13 +1,17 @@
 import bcrypt
-
+# Function to hash a password
 def hash_password(plain_text_password):
     # Encode the password to bytes, required by bcrypt
-    pass_bytes = plain_text_password.encode('utf-8')
+    password_bytes = plain_text_password.encode('utf-8')
     # Generate a salt and hash the password
     salt = bcrypt.gensalt()
-    hashed_pass = bcrypt.hashpw(pass_bytes, salt)
-    return hashed_pass
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    return hashed_password
 
-passwd = "secret"
-pass_hash = hash_password(passwd)
-print(f"Password: {passwd} Hash: {str(pass_hash)}")
+# Function to verify a password
+def verify_password(plain_password, hashed_password):
+    # Encoding both plain text and stored hash to bytes
+    password_bytes = plain_password.encode('utf-8')
+    hashed_password_bytes = hashed_password.encode('utf-8')
+    # bcrypt.checkpw handles extracting the salt and comparing
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
